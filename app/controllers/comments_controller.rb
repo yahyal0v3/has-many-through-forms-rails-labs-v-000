5a518e58f09d8feca98819f5d95a385c53e86e46
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-03-02 19:54:25 +0000

    automatically backed up by learn

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
 
   def create
     comment = Comment.create(comment_params)
-    raise comment.user,inspect
+    raise comment.user.inspect
     redirect_to comment.post
   end
 
